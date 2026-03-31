998c3d4dcbcb8f0f655aa69b1ead54476b29fcba
vasilyefff <vasilyeff_k@mail.ru>
2026-03-31 08:59:32 +0300

    feat: connect clients slice to store

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import clientsReducer from '@/entities/client/model/clientSlice'
 
 export const store = configureStore({
-  reducer: {}
+	reducer: {
+		clients: clientsReducer
+	}
 })
