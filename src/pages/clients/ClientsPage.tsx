c2bea35cb180c8111093346e51e2f6d18bb98904
vasilyefff <vasilyeff_k@mail.ru>
2026-04-01 14:53:11 +0300

    feat: add client via Redux dispatch

--- src/pages/clients/ClientsPage.tsx
+++ src/pages/clients/ClientsPage.tsx
@@ -1,9 +1,26 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { addClient } from '@/entities/client/model/clientSlice'
 
 export const ClientsPage = () => {
   const clients = useSelector((state) => state.clients.clients)
+  const dispatch = useDispatch()
 
   console.log(clients)
 
-  return <div>Clients Page</div>
+  const handleAdd = () => {
+    dispatch(
+      addClient({
+        id: Date.now().toString(),
+        name: 'Test',
+        email: 'test@mail.com',
+      }),
+    )
+  }
+
+  return (
+    <>
+      <div>Clients Page</div>
+      <button onClick={handleAdd}>Add client</button>
+    </>
+  )
 }
